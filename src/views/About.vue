09bb9991a64cf62bef080a497097662f7dddb10a
Lorenzo <ganzalorenzo7@gmail.com>
2026-05-28 22:59:33 +0200

    visit rwanda

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,28 @@
+<script setup></script>
 <template>
-   <section class="bg-white text-green-600 h-screen w-full mt-12">
-    <h1 class="text-5xl font-bold py-24 text-center">
-        Welcome To Rwanda</h1>
-        </section>
+  <section class="bg-black text-white py-24 px-6">
+    <div class="text-center mb-16">
+      <p class="text-yellow-400 uppercase tracking-[9px] mb-3">
+        about us</p>
+      <h1 class="text-5xl md:text-6xl font-bold">
+        Tour The Land Of A Thousand Hills, Rwanda🌍
+      </h1>
+    </div>
+    <div
+      class="-w-6xl mx-auto grid md:grid-cols-2 gap-12 items-center">
+      <div>
+        <img
+          src="/src/assets/photo 11.jpg"
+          class="w-full  object-cover rounded-2xl"/>
+      </div>
+      <div>
+        <h2 class="text-green-800 text-4xl font-bold mb-6">
+          Meet our team😎</h2>
+        <p class="text-yellow-400 mb-6">
+          Dedicated people with warm hearts to elcome new gems in the heart of Africa📸</p>
+        <p class="text-gray-400 mb-6">
+          15 years of shaping Rwanda's tourism and abroad. When someone needs to relax, we've got you🚙</p>
+      </div>
+    </div>
+  </section>
 </template>
--- src/views/home.vue
+++ src/views/home.vue
@@ -1,15 +1,54 @@
 <script setup></script>
 <template>
     <!-- Hero section -->
-     <section class="relative h-screen bg-cover bg-center flex items-center justify-center text-white"
-     style="background-image: url('/src/assets/photo 5.JPG');"
-     >
-     <div class="bg-black/40 bg-opacity-50 absolute inset-0">
-
-     </div>
-     <div class="relativ text-center px-4">
-        <h1 class="text-5xl font-bold mb-4">Welcome to Rwanda</h1>
-        <p class="text-xl mb-8 text-green-300">The land of a thousand hills</p>
-     </div>
+<section class="py-20 px-6 bg-black">
+<div class="text-center text-white mb-14">
+  <h2 class="text-5xl font-bold mb-3">
+VISIT RWANDA🌍  </h2>
+  <p class="text-green-400">
+    Discover beauty and adventure
+  </p>
+</div>
+<div class="grid md:grid-cols-3 gap-4 w-9xl">
+  <div
+    class="text-white">
+    <img
+      src="/src/assets/photo 8.jpg"
+      class="h-150 w-full object-cover"/>
+    <div class="p-5">
+      <h3 class="text-2xl font-bold mb-2">
+        Gallery</h3>
+      <p class="text-gray-300 text-sm">
+        Hills and Nature🏕
+      </p>
+    </div>
+  </div>
+  <div
+    class="text-white">
+    <img
+      src="/src/assets/location.jpeg"
+      class="h-150 w-full object-cover"/>
+    <div class="p-5">
+      <h3 class="text-2xl font-bold mb-2">
+        Services</h3>
+      <p class="text-gray-300 text-sm">
+        Travel, tours and fun tips🚀
+      </p>
+    </div>
+  </div>
+  <div
+    class="text-white">
+    <img
+      src="/src/assets/photo 9.jpeg"
+      class="h-150 w-full object-cover"/>
+    <div class="p-5">
+      <h3 class="text-2xl font-bold mb-2">
+        Destinations
+      </h3>
+      <p class="text-gray-300 text-sm">
+        Visit cool and amazing places🏝</p>
+    </div>
+  </div>
+</div>
 </section>
 </template>
